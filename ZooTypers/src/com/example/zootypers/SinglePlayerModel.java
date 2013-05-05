925c41fbd6585b0ee4d9665a34f5d7ed2c816fee
winglam <winglam@cs.washington.edu>
2013-05-05 13:08:56 -0700

    fixed bug where windows computers would still parse \r resulting in words not disappearing from screen

--- ZooTypers/src/com/example/zootypers/SinglePlayerModel.java
+++ ZooTypers/src/com/example/zootypers/SinglePlayerModel.java
@@ -145,7 +145,7 @@ public class SinglePlayerModel extends Observable {
 		} else if (wordsList[wordsDisplayed[currWordIndex]].charAt(currLetterIndex) == letter) {
 
 			// store length of current word
-			int wordLen = wordsList[wordsDisplayed[currWordIndex]].length();
+			int wordLen = wordsList[wordsDisplayed[currWordIndex]].trim().length();
 
 			// word is completed after final letter is typed
 			if ((currLetterIndex + 1) >= wordLen) {
