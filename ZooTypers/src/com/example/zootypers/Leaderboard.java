9958c703835f9c965fca484fab8511c2366caf08
David Liang <dyxliang@uw.edu>
2013-05-04 18:39:59 -0700

    Used CodePro to check the styles of all classes again and corrected bad styles according to our standards.

--- ZooTypers/src/com/example/zootypers/Leaderboard.java
+++ ZooTypers/src/com/example/zootypers/Leaderboard.java
@@ -13,18 +13,18 @@ import android.view.Window;
  */
 public class Leaderboard extends Activity {
 
-  @Override
-  protected final void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    setContentView(R.layout.activity_leaderboard);
-  }
+    @Override
+    protected final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_leaderboard);
+    }
 
-  @Override
-  public final boolean onCreateOptionsMenu(final Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.leaderboard, menu);
-    return true;
-  }
+    @Override
+    public final boolean onCreateOptionsMenu(final Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.leaderboard, menu);
+        return true;
+    }
 
 }
