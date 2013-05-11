6ba1bf80915ba952151bda9b995a06727c96d74d
dyxliang <dyxliang@uw.edu>
2013-05-10 17:30:19 -0700

    Created simple test stubs for Jenkins ant unittests build.

--- ZooTypers/tests/src/com/example/zootypers/test/TitlePageTest.java
+++ ZooTypers/tests/src/com/example/zootypers/test/TitlePageTest.java
@@ -3,26 +3,41 @@ package com.example.zootypers.test;
 import com.example.zootypers.TitlePage;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class TitlePageTest extends ActivityInstrumentationTestCase2<TitlePage> {
 
-//    public TitlePageTest(String pkg, Class<TitlePage> activityClass) {
-//        super(pkg, activityClass);
-//        // TODO Auto-generated constructor stub
-//    }
-
-    private TitlePage titleActivity;
-    
-    @SuppressWarnings("deprecation")
     public TitlePageTest() {
-        super("com.android.example.ZooTypers", TitlePage.class);
-      } // end of SpinnerActivityTest constructor definition
-
-    @Override
-    protected void setUp() throws Exception {
-      super.setUp();
-
-      setActivityInitialTouchMode(false);
-    } // end of setUp() method definition
+        super(TitlePage.class);
+    }
     
+    @SmallTest
+    public void testSinglePlayerButtonExists() {
+        assertNotNull(getActivity().findViewById(com.example.zootypers.R.id.single_player_button));
+    }
+    
+    @SmallTest
+    public void testMultiPlayerButtonExists() {
+        assertNotNull(getActivity().findViewById(com.example.zootypers.R.id.multiplayer_button));
+    }
+    
+    @SmallTest
+    public void testLeaderboardButtonExists() {
+        assertNotNull(getActivity().findViewById(com.example.zootypers.R.id.leaderboard_button));
+    }
+    
+    @SmallTest
+    public void testOptionsButtonExists() {
+        assertNotNull(getActivity().findViewById(com.example.zootypers.R.id.options_button));
+    }
+
+    @SmallTest
+    public void testSinglePlayerButtonDisplayCorrectText() {
+        assertNotNull(getActivity().findViewById(com.example.zootypers.R.id.single_player_button));
+        Button singlePlayerButton = (Button) getActivity().findViewById(com.example.zootypers.R.id.single_player_button);
+        singlePlayerButton.getText().equals("Single Player");
+    }
 }
