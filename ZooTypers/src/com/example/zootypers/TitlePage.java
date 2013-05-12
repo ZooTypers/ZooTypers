9138448e6b986f84d96335b44fba7cb7e3118dce
James Okada <jyo2@cs.washington.edu>
2013-05-12 02:29:43 -0700

    not done yet

--- ZooTypers/src/com/example/zootypers/TitlePage.java
+++ ZooTypers/src/com/example/zootypers/TitlePage.java
@@ -1,11 +1,16 @@
 package com.example.zootypers;
 
 import android.app.Activity;
+import android.app.ActionBar.LayoutParams;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.PopupWindow;
 
 /**
 *
@@ -15,6 +20,8 @@ import android.view.Window;
 */
 public class TitlePage extends Activity {
 
+	PopupWindow ppw; // for the multiplayer login popup
+	
     @Override
     protected final void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,5 +75,31 @@ public class TitlePage extends Activity {
     @Override
     public void onBackPressed() {
         // TODO trigger pause screen!
+    }
+    
+    /**
+    * Called when the user presses the "Multiplayer" button.
+    * Prompts a login screen
+    * @param view The button clicked
+    */
+    public final void multiplayerLogin(final View view) {
+    	// make the login popup screen with the login_popup layout
+    	LayoutInflater layoutInflater = 
+                 (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.login_popup, null);
+        ppw = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        ViewGroup parentLayout = (ViewGroup) findViewById(R.id.title_page_layout);
+        // set the position and size of popup
+        ppw.showAtLocation(parentLayout, Gravity.CENTER, 10, 20);
+        ppw.update(1000, 500);
+    }
+    
+    public void loginButton(View view) {
+    	ppw.dismiss();
+    }
+    
+    public void goToRegister(View view) {
+    	Intent registerIntent = new Intent(this, RegisterPage.class);
+    	startActivity(registerIntent);
     }
 }
