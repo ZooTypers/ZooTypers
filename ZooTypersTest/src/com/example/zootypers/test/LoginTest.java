44ad27e7a8c2ba9e819e8b9c5e69a5e25adcd557
dyxliang <dyxliang@uw.edu>
2013-05-16 18:39:21 -0700

    Adding in an separate test folder to see if it works.

---
+++ ZooTypersTest/src/com/example/zootypers/test/LoginTest.java
@@ -0,0 +1,73 @@
+package com.example.zootypers.test;
+
+import org.junit.*;
+import com.example.zootypers.*;
+import com.example.zootypers.R;
+import com.jayway.android.robotium.solo.Solo;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+/**
+ * Testing to see if the login feature works correctly by logging into
+ * an existing user, registering a new user, and also entering a password
+ * for recovering information.
+ * 
+ * (Black box testing to make sure that the login UI works.)
+ * 
+ * @author dyxliang
+ *
+ */
+
+public class LoginTest extends ActivityInstrumentationTestCase2<TitlePage> {
+	
+	private Solo solo;
+	private static final int TIMEOUT = 10000;
+	
+	public LoginTest() {
+		super(TitlePage.class);
+	}
+
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Test(timeout = TIMEOUT)
+	public void testingRegisteringForAccount() {
+		solo.clickOnButton("Multiplayer");
+		solo.clickOnButton("Register");
+		EditText username = (EditText) solo.getView(R.id.username_register_input);
+		solo.enterText(username, "David");
+		EditText password = (EditText) solo.getView(R.id.password_input_register);
+		solo.enterText(password, "12345");
+		EditText password2 = (EditText) solo.getView(R.id.confirm_password_input_register);
+		solo.enterText(password2, "12345");
+		EditText email = (EditText) solo.getView(R.id.email_input_register);
+		solo.enterText(email, "davidqwe123@hotmail.com");
+		solo.clickOnButton("Submit");
+	}
+	
+	@Test(timeout = TIMEOUT)
+	public void testingLoginToExistingUser() {
+		solo.clickOnButton("Multiplayer");
+		EditText username = (EditText) solo.getView(R.id.username_login_input);
+		solo.enterText(username, "David");
+		EditText password = (EditText) solo.getView(R.id.password_login_input);
+		solo.enterText(password, "1234567");
+		solo.clickOnButton("Login");
+		solo.goBack();
+		solo.clickOnButton("Logout");
+	}
+	
+	@Test(timeout = TIMEOUT)
+	public void testingForgotPasswordInput() {
+		solo.clickOnButton("Multiplayer");
+		EditText email = (EditText) solo.getView(R.id.email_forgot_password_input);
+		solo.enterText(email, "davidqwe123@hotmail.com");
+		solo.clickOnButton("Reset Password");
+	}
+	
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+}
---
+++ ZooTypersTest/src/com/example/zootypers/test/MultiPlayerModelTest.java
@@ -0,0 +1,19 @@
+package com.example.zootypers.test;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * Testing the multi-player model of the game.
+ * 
+ * (White box testing.)
+ * 
+ * TODO: tests
+ * 
+ * @author dyxliang
+ *
+ */
+
+public class MultiPlayerModelTest extends TestCase {
+
+}
