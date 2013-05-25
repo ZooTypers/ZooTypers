7e5ab8f21643379ca579d9e6eab754f4a00c7f02
David Liang <dyxliang@uw.edu>
2013-05-25 16:07:33 -0700

    Committing the new login tests.

--- ZooTypers/tests/src/com/example/zootypers/test/LoginTest.java
+++ ZooTypers/tests/src/com/example/zootypers/test/LoginTest.java
@@ -3,9 +3,10 @@ package com.example.zootypers.test;
 import org.junit.Test;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.Suppress;
+import android.test.UiThreadTest;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.zootypers.R;
 import com.example.zootypers.ui.TitlePage;
@@ -21,14 +22,11 @@ import com.jayway.android.robotium.solo.Solo;
  * @author dyxliang
  *
  */
-@Suppress
 public class LoginTest extends ActivityInstrumentationTestCase2<TitlePage> {
 
     private Solo solo;
     private static final int TIMEOUT = 10000;
     private Button multiButton;
-    private Button joinNow;
-    private Button loginButton;
 
     public LoginTest() {
         super(TitlePage.class);
@@ -42,8 +40,13 @@ public class LoginTest extends ActivityInstrumentationTestCase2<TitlePage> {
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
         multiButton = (Button) getActivity().findViewById(com.example.zootypers.R.id.multiplayer_button);
-        joinNow = (Button) getActivity().findViewById(com.example.zootypers.R.id.join_now);
-        loginButton = (Button) getActivity().findViewById(com.example.zootypers.R.id.login_button);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                multiButton.performClick();
+            }
+        });
+        solo.sleep(1000);
     }
 
     /**
@@ -51,40 +54,32 @@ public class LoginTest extends ActivityInstrumentationTestCase2<TitlePage> {
      */
     @Test(timeout = TIMEOUT)
     public void testingRegisteringForAccountInUse() {
-
+        final TextView joinNow = (TextView) solo.getView(com.example.zootypers.R.id.join_now);
+        solo.sleep(1000);
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                multiButton.performClick();
+                joinNow.performClick();
             }
         });
-        solo.sleep(3000);
-        
-//        getActivity().runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//                loginButton.performClick();
-//            }
-//        });
-//        solo.sleep(1000);
-
-        solo.clickOnText("Join now!");
+        solo.sleep(1000);
         EditText username = (EditText) solo.getView(R.id.username_register_input);
         solo.enterText(username, "David");
-        
         EditText password = (EditText) solo.getView(R.id.password_input_register);
         solo.enterText(password, "12345");
-        
         EditText password2 = (EditText) solo.getView(R.id.confirm_password_input_register);
         solo.enterText(password2, "12345");
-        
         EditText email = (EditText) solo.getView(R.id.email_input_register);
         solo.enterText(email, "davidqwe123@hotmail.com");
-        
-        solo.clickOnButton("Submit");
-        
+        final Button submitButton = (Button) solo.getView(com.example.zootypers.R.id.submit_register);
         solo.sleep(1000);
-        
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                submitButton.performClick();
+            }
+        });
+        solo.sleep(1000);
         solo.searchText("Username is already in use.");
     }
 
@@ -93,32 +88,57 @@ public class LoginTest extends ActivityInstrumentationTestCase2<TitlePage> {
      */
     @Test(timeout = TIMEOUT)
     public void testingLoginToExistingUserAndLoggingOut() {
-        solo.clickOnButton("Multiplayer");
         EditText username = (EditText) solo.getView(R.id.username_login_input);
         solo.enterText(username, "David");
         EditText password = (EditText) solo.getView(R.id.password_login_input);
         solo.enterText(password, "1234567");
-        solo.clickOnButton("Login");
-        solo.sleep(5000);
-        solo.clickOnButton("Main Menu");
-        solo.clickOnButton("Logout");
-        solo.searchText("Logged Out");
+        final Button loginButton = (Button) solo.getView(com.example.zootypers.R.id.login_button);
+        solo.sleep(1000);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                loginButton.performClick();
+            }
+        });
+        final Button logoutButton = (Button) solo.getView(com.example.zootypers.R.id.logout_button);
+        solo.sleep(1000);
+        solo.searchText("You are logged in as David");
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                logoutButton.performClick();
+            }
+        });
+        solo.searchText("You have successfully logged out!");
     }
 
-    //    /**
-    //     * Goes to Multiplayer activity and request an invalid email for a password reset.
-    //     */
-    //	@Test(timeout = TIMEOUT)
-    //	public void testingForgotPasswordInputFailure() {
-    //	    solo.clickOnButton("Multiplayer");
-    //		solo.clickOnText("Forgot your password?");
-    //		EditText email = (EditText) solo.getView(R.id.email_forgot_password_input);
-    //		solo.enterText(email, "davidqwe123@hotmail.com");
-    //		solo.clickOnButton("Reset");
-    //	}
+    /**
+     * Goes to Multiplayer activity and request an invalid email for a password reset.
+     */
+    @Test(timeout = TIMEOUT)
+    public void testingForgotPasswordInputFailure() {
+        final TextView forgotButton = (TextView) solo.getView(com.example.zootypers.R.id.forgot_pw);
+        solo.sleep(1000);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                forgotButton.performClick();
+            }
+        });
+        EditText email = (EditText) solo.getView(R.id.email_forgot_password_input);
+        solo.enterText(email, "davidqwe123@hotmail.com");
+        solo.sleep(1000);
+        final Button resetButton = (Button) solo.getView(com.example.zootypers.R.id.reset_password_button);
+        solo.sleep(1000);
+//        getActivity().runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                resetButton.performClick();
+//            }
+//        });
+    }
 
     protected void tearDown() throws Exception {
         solo.finishOpenedActivities();
     }
-
 }
