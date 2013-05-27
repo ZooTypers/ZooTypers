34018bbdf6fe9d82f274cc77db89c04510c1b69e
David Liang <davidliangcst@gmail.com>
2013-05-27 11:34:37 -0700

    Multiplayer & Leaderboard test stubs.

---
+++ ZooTypers/tests/src/com/example/zootypers/test/LeaderboardSPModelTest.java
@@ -0,0 +1,24 @@
+package com.example.zootypers.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeaderboardSPModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
